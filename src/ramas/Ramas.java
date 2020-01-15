39e03bda3224a99a1af920cd161348f5f5234816
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2020-01-15 14:03:59 +0100

    A Inicio

---
+++ src/ramas/Ramas.java
@@ -0,0 +1,13 @@
+package ramas;
+
+/**
+ *
+ * @author agonzalezacevedo
+ */
+public class Ramas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
